9587076acac4878816eeb3e1005ee0a0e98be660
Nguyễn Hải Dương <96025213+VincentHizelNguyen@users.noreply.github.com>
2023-04-30 22:14:29 +0700

    Update Cart.java

--- Lab02/Project/Cart.java
+++ Lab02/Project/Cart.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Collections;
-
 public class Cart {
     public static final int MAX_NUMBER_ORDERED = 20;
     private ArrayList<DigitalVideoDisc> itemsOrdered;
@@ -22,28 +20,6 @@ public class Cart {
         itemsOrdered.clear();
     }
 
-    public void sortCartByTitle() {
-        Collections.sort(itemsOrdered, new Comparator<DigitalVideoDisc>() {
-            public int compare(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
-                if (disc1.getTitle().compareToIgnoreCase(disc2.getTitle()) == 0) {
-                    return Double.compare(disc2.getCost(), disc1.getCost());
-                }
-                return disc1.getTitle().compareToIgnoreCase(disc2.getTitle());
-            }
-        });
-    }
-
-    public void sortCartByCost() {
-        Collections.sort(itemsOrdered, new Comparator<DigitalVideoDisc>() {
-            public int compare(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
-                if (disc1.getCost() == disc2.getCost()) {
-                    return disc1.getTitle().compareToIgnoreCase(disc2.getTitle());
-                }
-                return Double.compare(disc2.getCost(), disc1.getCost());
-            }
-        });
-    }
-
     public float totalCost() {
         float total = 0;
         for (DigitalVideoDisc disc : itemsOrdered) {
@@ -52,16 +28,7 @@ public class Cart {
         return total;
     }
 
-    public DigitalVideoDisc searchByTitle(String title) {
-        for (DigitalVideoDisc disc : itemsOrdered) {
-            if (disc.getTitle().toLowerCase().contains(title.toLowerCase())) {
-                return disc;
-            }
-        }
-        return null;
-    }
-
-    public int getQtyOrdered(DigitalVideoDisc disc) {
+   public int getQtyOrdered(DigitalVideoDisc disc) {
         int qty = 0;
         for (DigitalVideoDisc item : itemsOrdered) {
             if (item.equals(disc)) {
